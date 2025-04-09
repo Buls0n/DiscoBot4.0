7ab1e4e4c94734ca341b5f40fa418e6204073f70
Buls0n <asderpl0@gmail.com>
2025-04-09 02:44:12 +0200

    Update nightcore.js

--- commands/nightcore.js
+++ commands/nightcore.js
@@ -1,47 +1,38 @@
+// Final nightcore.js for Railway deployment with play-dl
 const { getQueue } = require('../utils/musicUtils');
-const { applyNightcoreEffect } = require('../utils/audioEffects');
 
 module.exports = {
   name: 'nightcore',
-  description: 'Applies a nightcore effect by increasing the pitch and speed of the audio',
+  description: 'Toggles the nightcore effect (increased pitch/speed)',
   execute(message, args) {
     const queue = getQueue(message.client, message.guild.id);
     
     if (!queue.connection) {
-      return message.reply('There is no song currently playing!');
-    }
-    
-    if (!message.member.voice.channel) {
-      return message.reply('You need to be in a voice channel to use this command!');
+      return message.reply('I\'m not currently playing anything!');
     }
     
     // Toggle nightcore effect
     queue.nightcoreEnabled = !queue.nightcoreEnabled;
     
-    // In a real implementation, we would apply audio filters here
-    // This would require additional audio processing libraries
-    applyNightcoreEffect(null, queue.nightcoreEnabled);
-    
-    // Create rich embed for nightcore effect
-    const embed = {
-      color: queue.nightcoreEnabled ? 0xf1c40f : 0x95a5a6,
-      title: queue.nightcoreEnabled ? '🎛️ Nightcore Effect Enabled' : '🎛️ Nightcore Effect Disabled',
-      description: queue.nightcoreEnabled ? 
-        'Music will now play with increased pitch and speed!' : 
-        'Music will now play at normal pitch and speed.',
-      footer: { text: `Requested by ${message.author.tag}` }
-    };
-    
-    if (queue.currentSong && queue.currentSong.thumbnail) {
-      embed.thumbnail = { url: queue.currentSong.thumbnail };
-    }
-    
-    message.channel.send({ embeds: [embed] });
-    
-    // Note: For the effect to actually apply to the current song,
-    // we would need to restart playback with the new effect settings
+    // Notify user of the change
     if (queue.nightcoreEnabled) {
-      message.channel.send('*Note: The effect will apply to the next song or when the current song is restarted.*');
+      message.channel.send({
+        embeds: [{
+          color: 0xf1c40f,
+          title: '🎛️ Nightcore Effect',
+          description: 'Nightcore effect has been **enabled**!',
+          footer: { text: 'Effect will apply to the next song' }
+        }]
+      });
+    } else {
+      message.channel.send({
+        embeds: [{
+          color: 0xf1c40f,
+          title: '🎛️ Nightcore Effect',
+          description: 'Nightcore effect has been **disabled**!',
+          footer: { text: 'Effect will apply to the next song' }
+        }]
+      });
     }
   }
 };
